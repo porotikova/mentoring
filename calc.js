142ff9422a043158480959034981299ce636d947
RomanAchilov <dakaktak95@gmail.com>
2019-12-09 21:28:24 +0300

    oop calc

--- calc.js
+++ calc.js
@@ -1,41 +1,70 @@
-const Calculator = (() => {
-  let state = 0;
+class Calculator {
+  constructor() {
+    this.state = 0;
+  }
 
-  return {
-    add: function(value) {
-      state += value;
-      return this;
-    },
-
-    subtract: function(value) {
-      state -= value;
-      return this;
-    },
-
-    divide: function(value) {
-      state /= value;
-      return this;
-    },
-
-    multiply: function(value) {
-      state *= value;
-      return this;
-    },
-
-    getResult: function() {
-      return state;
-    },
-
-    reset: function() {
-      state = 0;
-      return this;
-    },
-
-    setState: function(value) {
-      state = value;
-    },
-    fetchData: function(callback) {
-      callback.call(this);
+  add = (value = 0) => {
+    if (!this.add.value) {
+      this.add.value = this.state;
     }
+    this.add.value += value;
+    this.add.toString = () => this.add.value;
+    this.state = this.add;
+    return this.add;
   };
-})();
+
+  subtract = (value = 0) => {
+    if (!this.subtract.value) {
+      this.subtract.value = this.state;
+    }
+    this.subtract.value -= value;
+    this.subtract.toString = () => this.subtract.value;
+    this.state = this.subtract;
+    return this.subtract;
+  };
+
+  divide = (value = 0) => {
+    if (!this.divide.value) {
+      this.divide.value = this.state;
+    }
+    this.divide.value /= value;
+    this.divide.toString = () => this.divide.value;
+    this.state = this.divide;
+    return this.divide;
+  };
+
+  multiply = (value = 0) => {
+    if (!this.multiply.value) {
+      this.multiply.value = this.state;
+    }
+    this.multiply.value *= value;
+    this.multiply.toString = () => this.multiply.value;
+    this.state = this.multiply;
+    return this.multiply;
+  };
+
+  getResult() {
+    return this.state;
+  }
+
+  reset() {
+    this.state = 0;
+    return this.state;
+  }
+
+  setState = (value = 0) => {
+    if (!this.setState.value) {
+      this.setState.value = this.state;
+    }
+    this.setState.value = value;
+    this.setState.toString = () => this.setState.value;
+    this.state = this.setState;
+    return this.setState;
+  };
+
+  fetchData = callback => {
+    callback.call(this);
+  };
+}
+
+const calc = new Calculator();
